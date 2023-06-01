a3a7f5312c06d71ce7df0b6792ac037e9fdfb6fc
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2023-06-02 03:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ var n = Math.random();
 n = n * 100;
 n = Math.floor(n);
 document.querySelector(".btn").innerHTML=n+"%";
+document.querySelector("body").addEventListener("click",function(){
+    var audio = new Audio("Kali Kali Zulfon Ke - Madhur Sharma.mp3");
+    audio.play();
+}, {once : true});
